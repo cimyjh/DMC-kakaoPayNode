d30c2716dc2daa92bc43b9b46fdf25ab60636c8d
cimyjh@gmail.com <developer123@yjh>
2020-08-12 17:19:18 +0900

    gitignore

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+// const { mongoURI } = require("./prod");
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://cimyjh:developermongo@dmc-init.5g37d.mongodb.net/DMC-init?retryWrites=true&w=majority",
+};
